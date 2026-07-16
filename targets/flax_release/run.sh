e986cbd74f44e7c08c0e18f93ab5890d1f1ea665
Pakize Sanal <pakize.sanal@amd.com>
2026-07-16 09:27:01 -0500

    Change XLA_PYTHON_CLIENT_ALLOCATOR from platform to bfc

--- targets/flax_release/run.sh
+++ targets/flax_release/run.sh
@@ -57,7 +57,7 @@ pip3 install -r "${REQ_FILE}"
 export PY_COLORS=1
 export TF_CPP_MIN_LOG_LEVEL=0
 export JAX_ENABLE_X64=0
-export XLA_PYTHON_CLIENT_ALLOCATOR=platform
+export XLA_PYTHON_CLIENT_ALLOCATOR=bfc
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
 
 MODEL_RUN_STARTED_AT="$(date -u +%Y-%m-%dT%H:%M:%SZ)"
--- targets/maxtext_release/run.sh
+++ targets/maxtext_release/run.sh
@@ -40,7 +40,7 @@ done
 export PY_COLORS=1
 export TF_CPP_MIN_LOG_LEVEL=0
 export JAX_ENABLE_X64=0
-export XLA_PYTHON_CLIENT_ALLOCATOR=platform
+export XLA_PYTHON_CLIENT_ALLOCATOR=bfc
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
 
 MODEL_RUN_STARTED_AT="$(date -u +%Y-%m-%dT%H:%M:%SZ)"
